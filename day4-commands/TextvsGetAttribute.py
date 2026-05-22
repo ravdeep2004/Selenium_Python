3da07d6d9fa11e6776682f176fa17d362d0b8302
Ravdeep Singh <ravdeep2004@gmail.com>
2026-05-22 12:17:05 +0530

    Added difference b/w Text() and GetAttribute()

---
+++ day4-commands/TextvsGetAttribute.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://admin-demo.nopcommerce.com/login")
+driver.maximize_window()
+
+
+# Example with textbox
+
+# emailbox = driver.find_element(By.XPATH, "//input[@id='Email']")
+
+# emailbox.clear()
+# emailbox.send_keys("abc@gmail.com")
+
+# print("result of text:", emailbox.text)   # printed nothing
+# print("result of get_attribute():",
+#       emailbox.get_attribute('value'))    # abc@gmail.com
+
+
+button = driver.find_element(
+    By.XPATH,
+    "//button[normalize-space()='Log in']"
+)
+
+print("result of text:", button.text)
+print("result of get_attribute():", button.get_attribute('value'))
+print("result of get_attribute():", button.get_attribute('type'))
+
+driver.quit()
